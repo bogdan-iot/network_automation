b713fbe19461e2d367e64f8ddd342850581911cf
Bogdan Radu <bogdan@iot-elite.com>
2024-11-25 21:56:30 +0100

    Added long_description

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1.0',
     packages=['src'],
     description='Network Automation Library',
+    long_description="This is a library for Network Automation",
     author='Bogdan Radu',
     setup_requires=["pytest"]
 )
