019d16438623d162bc5da84bc498eb9e89d68cd9
Bogdan Radu <bogdan@iot-elite.com>
2024-11-25 11:25:36 +0100

    Added pyproject.toml, refactored folder structure

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='network_automation',
-    packages=find_packages(include=['network_automation']),
+    name='network-automation',
     version='0.1.0',
+    packages=['src'],
     description='Network Automation Library',
     author='Bogdan Radu',
-    setup_requires=['pytest>=8.3'],
-    tests_require=['pytest>=8.3'],
-    test_suite='tests'
 )
