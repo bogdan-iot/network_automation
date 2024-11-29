fe65a4d482349fc03a4af47ab87236d5702a2219
Bogdan Radu <bogdan@iot-elite.com>
2024-11-29 19:15:34 +0100

    Adjusted timeouts for executing commands

--- src/network_automation/cisco.py
+++ src/network_automation/cisco.py
@@ -28,24 +28,26 @@ class CiscoSSHDevice(object):
         }
         self.conn = ConnectHandler(**netmiko_device)
 
-    def execute_command(self, command, parse=True):
+    def execute_command(self, command, parse=True, timeout=10):
         """
         This method executes a command on Cisco CLI and returns the result
         :param command: The command to run
         :param parse: Parse the output with textfsm (True)
+        :param timeout: Set the timeout for executing the command and getting the result
         :return:
         """
         if parse:
-            return self.conn.send_command(command, use_textfsm=True)
+            return self.conn.send_command(command, use_textfsm=True, read_timeout=timeout)
 
         return self.conn.send_command(command)
 
-    def get_interface_details(self):
+    def get_interface_details(self, timeout=30):
         """
         This method executes the 'show interface' command and returns the result parsed with textfsm
+        :param timeout: Set the timeout for executing the command and getting the result
         :return:
         """
-        interfaces = self.execute_command('show interface')
+        interfaces = self.execute_command('show interface', timeout=timeout)
         return [MyDict(x) for x in interfaces]
 
     def get_device_serial(self):
