59c16b86dd0c85a4b240c9617ac8a9a777c5247e
Bogdan Radu <bogdan@iot-elite.com>
2024-11-29 17:26:00 +0100

    appending path

--- tests/test_cisco.py
+++ tests/test_cisco.py
@@ -1,9 +1,3 @@
-import sys
-from pathlib import Path
-
-# Add 'src' directory to Python's module search path
-sys.path.append(str(Path(__file__).resolve().parent.parent / "src"))
-
 import json
 import os
 import unittest
