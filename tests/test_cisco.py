3c5f1f25406e808348d981b3ada232a87e91febf
Bogdan Radu <bogdan@iot-elite.com>
2024-11-29 17:24:08 +0100

    Fixed module paths

--- tests/test_cisco.py
+++ tests/test_cisco.py
@@ -1,3 +1,9 @@
+import sys
+from pathlib import Path
+
+# Add 'src' directory to Python's module search path
+sys.path.append(str(Path(__file__).resolve().parent.parent / "src"))
+
 import json
 import os
 import unittest
